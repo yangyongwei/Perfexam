c332ce2495e7b3c459eba735eb60db521f27dfae
yangyongwei <yyw0080@sina.com>
2018-06-19 12:05:16 +0800

    二级菜单级联

---
+++ src/main/java/cn/piesat/exam/service/DeptService.java
@@ -0,0 +1,23 @@
+package cn.piesat.exam.service;
+
+import cn.piesat.exam.domain.Dept;
+import cn.piesat.exam.mapper.DeptMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DeptService {
+
+    @Autowired
+    DeptMapper deptMapper;
+
+    public List<Dept> findAllDept(){
+        return deptMapper.findAllDept();
+    }
+
+    public Dept findDeptById(int id){
+        return deptMapper.findDeptById(id);
+    }
+}
