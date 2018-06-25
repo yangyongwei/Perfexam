bc9c5d743f55b99e5795ce10b0e7637ac1ff5cc3
DESKTOP-TNKAPGJ\Lion <yyw0080@sina.com>
2018-06-25 17:53:35 +0800

    添加注册用户重名验证

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,22 +1,10 @@
 $(function () {
 
+    // 2秒后消失提示框
+    setTimeout(
+        function () {
+            $(".alert").alert('close');
+        }, 2000
+    );
 });
-
-// <script>
-// function showPopover(target, msg) {
-//     target.attr("data-original-title", msg);
-//     $('[data-toggle="tooltip"]').tooltip();
-//     target.tooltip('show');
-//     target.focus();
-//
-//     //2秒后消失提示框
-//     var id = setTimeout(
-//         function () {
-//             target.attr("data-original-title", "");
-//             target.tooltip('hide');
-//         }, 2000
-//     );
-// }
-// showPopover($("#tableName"),"表名已存在");
-// </script>
 
