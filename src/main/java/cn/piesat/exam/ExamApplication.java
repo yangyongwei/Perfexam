c66c825ec188ce7abcabb82cb8108797e9bf8724
yangyongwei <yyw0080@sina.com>
2018-07-08 14:26:17 +0800

    添加mybatis generator配置！

--- src/main/java/cn/piesat/exam/ExamApplication.java
+++ src/main/java/cn/piesat/exam/ExamApplication.java
@@ -2,10 +2,9 @@ package cn.piesat.exam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-@MapperScan("cn.piesat.exam.mapper")
+//@MapperScan("cn.piesat.exam.mapper")
 public class ExamApplication {
 
     public static void main(String[] args) {
