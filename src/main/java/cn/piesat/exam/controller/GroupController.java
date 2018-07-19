5b4f9202160a9a4a7b6942813168548ef71ab0f6
DESKTOP-TNKAPGJ\Lion <yyw0080@sina.com>
2018-07-19 16:32:53 +0800

    添加注册用户重名验证

--- src/main/java/cn/piesat/exam/controller/GroupController.java
+++ src/main/java/cn/piesat/exam/controller/GroupController.java
@@ -25,21 +25,6 @@ public class GroupController {
     @Autowired
     private DeptGroupService deptGroupService;
 
-//    @GetMapping("/deptby")
-//    public List<String> GroupListByDeptId(@RequestParam(value="id") Integer id)
-//    {
-//        List<String> groupNames = new ArrayList();
-//        List<Integer>  groupIds = deptGroupService.findGroupIdsByDeptId(id);
-//        if(groupIds.isEmpty()){
-//            return groupNames;
-//        }
-//        List<Group> groupList = groupService.findGroupByIds(groupIds);
-//        for(Group group : groupList){
-//            groupNames.add(group.getGroupName());
-//        }
-//        return groupNames;
-//    }
-
     @GetMapping("/findGroupBy")
     public List<String> GroupListByDeptName(@RequestParam(value="deptName") String deptName)
     {
